19fd702f727184952a80ea0df22ac295c03d8366
M1sterChaos <austinbuck70@gmail.com>
2022-04-20 14:56:29 -0500

    Prototype

---
+++ Prototype/CIS350Assignment8Prototype/Assets/New Scripts/Target.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * (Austin Buck)
+ * (Assignment 8)
+ * (Controls the enemy and adds or removes score)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    [SerializeField]
+    private int pointValue;
+
+    public ParticleSystem explosionParticle;
+
+    private gameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<gameManager>();
+
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+    private void OnMouseDown()
+    {
+        gm.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gm.GameOver();
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Prototype/CIS350Assignment8Prototype/Assets/New Scripts/gameManager.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * (Austin Buck)
+ * (Assignment 8)
+ * (Controls the game and score)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+
+    public List<GameObject> targets;
+
+    private float spawnRate = 1f;
+
+    public TextMeshProUGUI scoreText;
+
+    public TextMeshProUGUI goText;
+
+    public bool gameOver = false;
+
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+
+        scoreText.text = "Score: " + score;
+        goText.gameObject.SetActive(false);
+    }
+    public void GameOver()
+    {
+        goText.text = "Game Over \nPress R to Restart!";
+        goText.gameObject.SetActive(true);
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(gameOver)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            int index = Random.Range(0, targets.Count);
+
+            Instantiate(targets[index]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
